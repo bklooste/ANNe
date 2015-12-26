e8173b97c2b03348c6477c5ceeead3bdcf332860
ben <bklooste@gmail.com>
2015-12-26 18:06:49 +1100

    working on more tests

--- tests/network_specs.rs
+++ tests/network_specs.rs
@@ -7,30 +7,41 @@ use anne::core::{Block};
 
 
     #[test]
-     fn fullmesh_integration ()
+     fn fullmesh_integration_w0 ()
      {
          unsafe
          {
-             static input_buf: &'static [f32] = &[1f32, 2f32, 3f32, 4f32, 5f32];
-             static mut output_buf: & 'static mut [f32] = & mut [1f32, 2f32, 3f32, 4f32, 5f32];
-             static  weights: & 'static  [f32] = & [ 0f32  ; 5];
+             static INPUT_BUF: &'static [f32] = &[1f32, 2f32, 3f32, 4f32, 5f32];
+             static mut OUTPUT_BUF: & 'static mut [f32] = & mut [1f32, 2f32, 3f32, 4f32, 5f32];
+             static  WEIGHTS: & 'static  [f32] = & [ 0f32  ; 5];
 
              let mut block_data = BlockData::new(5);
              block_data.neuron_count = 5;
              block_data.synapse_count = 5;
 
-             let mut block  =  LogisticBlock::new(block_data
-                     , weights
-                     , output_buf
-                     , input_buf
-             );
-
+             let mut block = LogisticBlock::new(block_data, WEIGHTS, OUTPUT_BUF, INPUT_BUF);
              block.process_buffers();
-             // assert output is correct
 
-             assert_eq!(output_buf, & [0.5f32 ;5]);
-
-
-
+             assert_eq!(OUTPUT_BUF, & [0.5f32 ;5]);
          }// unsafe
      }
+
+     #[test]
+      fn fullmesh_integration_w05 ()
+      {
+          unsafe
+          {
+              static INPUT_BUF: &'static [f32] = &[1f32, 2f32, 3f32, 4f32, 5f32];
+              static mut OUTPUT_BUF: & 'static mut [f32] = & mut [1f32, 2f32, 3f32, 4f32, 5f32];
+              static  WEIGHTS: & 'static  [f32] = & [ 0.5f32  ; 5];
+
+              let mut block_data = BlockData::new(5);
+              block_data.neuron_count = 5;
+              block_data.synapse_count = 5;
+
+              let mut block = LogisticBlock::new(block_data, WEIGHTS, OUTPUT_BUF, INPUT_BUF);
+              block.process_buffers();
+
+              assert_eq!(OUTPUT_BUF, & [0.99944717f32 ;5]);
+          }// unsafe
+      }
